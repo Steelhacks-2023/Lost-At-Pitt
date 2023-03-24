3c03f14925dd8a858d793ebcb1d37c5ee2ff9f8d
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2023-03-24 19:32:14 -0400

    Added Google Maps visibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:lost_found_steelhacks/mapPage.dart';
 import 'firebase_options.dart';
 
 // ...
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: mapPage(),
     );
---
+++ lib/mapPage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart'
+    as GWebMap;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class mapPage extends StatefulWidget {
+  const mapPage({super.key});
+
+  @override
+  State<mapPage> createState() => _mapPageState();
+}
+
+class _mapPageState extends State<mapPage> {
+  late GoogleMapController mapController;
+
+  final Stream<QuerySnapshot> _lostCollectionStream =
+      FirebaseFirestore.instance.collection('Lost').snapshots();
+
+  //University of Pittsburgh Coordinates
+  final LatLng _center = const LatLng(40.4443533, -79.960835);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _lostCollectionStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something Has Gone Wrong Please Refresh");
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Waiting Getting Data");
+          }
+          
+          return Scaffold(
+              body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 16,
+            ),
+          ));
+        });
+  }
+}
