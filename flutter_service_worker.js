59968b22ccbccf74292c482f741ea0c756aeb605
jeffzheng13 <jeffreyzheng12@gmail.com>
2023-03-25 20:07:08 +0000

    itemRequest update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "815e5bfba57113ae9990a282f5248c67",
+"main.dart.js": "06ebdb1bb6d1166b49be6001ff511c76",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -24378,9 +24378,9 @@ const RESOURCES = {
 "assets/packages/icon_forest/assets/fonts/flat_icons_arrows.ttf": "92dcd169306f1506375b54ed45f6c0e8",
 "assets/packages/icon_forest/assets/fonts/kicons_aliyun.ttf": "007946e2afd8d3363450351612880fb5",
 "assets/FontManifest.json": "6708c18f769ddfe5a8025815293491e0",
-"assets/NOTICES": "db70d8bbe2979e5a1bbe3ef7846ee0eb",
-"index.html": "09dad225bfcecac2ebd560f1ef565fc9",
-"/": "09dad225bfcecac2ebd560f1ef565fc9",
+"assets/NOTICES": "af432217949ec2c5394bb067acc05cf4",
+"index.html": "33f009b647f66798253dd91c798712ba",
+"/": "33f009b647f66798253dd91c798712ba",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
