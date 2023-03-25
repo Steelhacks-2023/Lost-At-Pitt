d6cd168415fbcd416459c55f39255695a3a8b1ea
jeffzheng13 <jeffreyzheng12@gmail.com>
2023-03-25 20:45:05 +0000

    final changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "f897bc550541c3fe154297c97bad45c9",
+"main.dart.js": "fe7f58eddcb270b6a8cc59edef66132b",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -24379,8 +24379,8 @@ const RESOURCES = {
 "assets/packages/icon_forest/assets/fonts/kicons_aliyun.ttf": "007946e2afd8d3363450351612880fb5",
 "assets/FontManifest.json": "6708c18f769ddfe5a8025815293491e0",
 "assets/NOTICES": "af432217949ec2c5394bb067acc05cf4",
-"index.html": "fc3b831482117023b8f64a635840947a",
-"/": "fc3b831482117023b8f64a635840947a",
+"index.html": "14b3582483453084c88bd30a1cfc172b",
+"/": "14b3582483453084c88bd30a1cfc172b",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
