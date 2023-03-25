bec6306639ed205faaa3392e2679d0e56ff11e06
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2023-03-25 09:51:19 -0400

    Added lost marker functionality

--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -55,6 +55,9 @@ class _mapPageState extends State<mapPage> {
   final Stream<QuerySnapshot> _lostCollectionStream =
       FirebaseFirestore.instance.collection('Lost').snapshots();
 
+  final Stream<QuerySnapshot> _foundCollectionStream =
+      FirebaseFirestore.instance.collection('Found').snapshots();
+
   //University of Pittsburgh Coordinates
   final LatLng _center = const LatLng(40.4443533, -79.960835);
 
@@ -65,144 +68,152 @@ class _mapPageState extends State<mapPage> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-        stream: _lostCollectionStream,
+        stream: _foundCollectionStream,
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text("Something Has Gone Wrong Please Refresh");
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Waiting Getting Data");
-          }
+          return StreamBuilder<QuerySnapshot>(
+              stream: _lostCollectionStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text("Something Has Gone Wrong Please Refresh");
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return Text("Waiting Getting Data");
+                }
 
-          //List<LostObject> lostObjects = [];
-          //query database, go through each item in database, and create list of lost objects
-          for (int i = 0; i < snapshot.data!.size; i++) {
-            QueryDocumentSnapshot singleDoc = snapshot.requireData.docs[i];
-            //LostObject lostItem = LostObject.fromFirestore(singleDoc, null);
-            print(singleDoc.data());
-          }
+                List<LostObject> lostObjects = [];
+                //query database, go through each item in database, and create list of lost objects
+                for (int i = 0; i < snapshot.data!.size; i++) {
+                  QueryDocumentSnapshot singleDoc =
+                      snapshot.requireData.docs[i];
+                  var data = singleDoc.data() as Map;
+                  //print(data["Description"].toString() + data["Phone"].toString());
+                  LostObject lostItem =
+                      LostObject.fromFirestore(singleDoc, null);
+                  lostObjects.add(lostItem);
+                  GeoPoint geo = data["Location"];
+                  //_add(geo.latitude, geo.longitude);
+                  final MarkerId temp = MarkerId(singleDoc.id);
+                  final Marker marker = Marker(
+                    markerId: temp,
+                    icon: markerIcon,
+                    position: LatLng(geo.latitude, geo.longitude),
+                    //infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+                    // onTap: () {
+                    //   _onMarkerTapped(markerId);
+                    // },
+                  );
+                  markers[temp] = marker;
+                }
 
-          PanelController _panelController = PanelController();
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("Lost and Found"),
-            ),
-            body:
-            SlidingUpPanel(
-              // defaultPanelState: PanelState.CLOSED,
-              // minHeight: 0,
-              controller: _panelController,
-              backdropEnabled: true,
-              collapsed: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.blueGrey,
+                PanelController _panelController = PanelController();
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text("Lost and Found"),
                   ),
-                  child: const Center(
-                      child: Text(
-                    "what should go here?",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-              panel: FormWidget(),
-              body:
-              GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 16,
-              ),
-              onTap: (coords) {
-                _add(coords.latitude, coords.longitude);
-                _panelController.open();
-                
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //         builder: (context) => SwitchApp(coord: coords)));
-              },
-              markers: Set<Marker>.of(markers.values),
-            ),
-            ),
+                  body: SlidingUpPanel(
+                    // defaultPanelState: PanelState.CLOSED,
+                    // minHeight: 0,
+                    controller: _panelController,
+                    backdropEnabled: true,
+                    collapsed: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.blueGrey,
+                        ),
+                        child: const Center(
+                            child: Text(
+                          "what should go here?",
+                          style: TextStyle(color: Colors.white),
+                        ))),
+                    panel: FormWidget(),
+                    body: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: _center,
+                        zoom: 16,
+                      ),
+                      onTap: (coords) {
+                        _add(coords.latitude, coords.longitude);
+                        _panelController.open();
 
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => SwitchApp(coord: coords)));
+                      },
+                      markers: Set<Marker>.of(markers.values),
+                    ),
+                  ),
+                );
 
-            
+                //       body: Stack(alignment: Alignment.center, children: [
 
-          );
+                //     SlidingUpPanel(
+                //       // defaultPanelState: PanelState.CLOSED,
+                //       // minHeight: 0,
+                //       controller: PanelController(),
+                //       backdropEnabled: true,
+                //       collapsed: Container(
+                //           decoration: const BoxDecoration(
+                //             color: Colors.blueGrey,
+                //           ),
+                //           child: const Center(
+                //               child: Text(
+                //             "what should go here?",
+                //             style: TextStyle(color: Colors.white),
+                //           ))),
+                //       panel: FormWidget(),
+                //     ),
 
+                //     GoogleMap(
+                //       initialCameraPosition: CameraPosition(
+                //         target: _center,
+                //         zoom: 16,
+                //       ),
+                //       onTap: (coords) {
+                //         _add(coords.latitude, coords.longitude);
 
+                //         // Navigator.push(
+                //         //     context,
+                //         //     MaterialPageRoute(
+                //         //         builder: (context) => SwitchApp(coord: coords)));
+                //       },
+                //       markers: Set<Marker>.of(markers.values),
+                //     ),
 
-            
-        //       body: Stack(alignment: Alignment.center, children: [
+                //     SlidingUpPanel(
+                //       // defaultPanelState: PanelState.CLOSED,
+                //       // minHeight: 0,
+                //       controller: PanelController(),
+                //       backdropEnabled: true,
+                //       collapsed: Container(
+                //           decoration: const BoxDecoration(
+                //             color: Colors.blueGrey,
+                //           ),
+                //           child: const Center(
+                //               child: Text(
+                //             "what should go here?",
+                //             style: TextStyle(color: Colors.white),
+                //           ))),
+                //       panel: FormWidget(),
+                //     ),
 
-            
-        //     SlidingUpPanel(
-        //       // defaultPanelState: PanelState.CLOSED,
-        //       // minHeight: 0,
-        //       controller: PanelController(),
-        //       backdropEnabled: true,
-        //       collapsed: Container(
-        //           decoration: const BoxDecoration(
-        //             color: Colors.blueGrey,
-        //           ),
-        //           child: const Center(
-        //               child: Text(
-        //             "what should go here?",
-        //             style: TextStyle(color: Colors.white),
-        //           ))),
-        //       panel: FormWidget(),
-        //     ),
-
-
-              
-        //     GoogleMap(
-        //       initialCameraPosition: CameraPosition(
-        //         target: _center,
-        //         zoom: 16,
-        //       ),
-        //       onTap: (coords) {
-        //         _add(coords.latitude, coords.longitude);
-                
-        //         // Navigator.push(
-        //         //     context,
-        //         //     MaterialPageRoute(
-        //         //         builder: (context) => SwitchApp(coord: coords)));
-        //       },
-        //       markers: Set<Marker>.of(markers.values),
-        //     ),
-
-        //     SlidingUpPanel(
-        //       // defaultPanelState: PanelState.CLOSED,
-        //       // minHeight: 0,
-        //       controller: PanelController(),
-        //       backdropEnabled: true,
-        //       collapsed: Container(
-        //           decoration: const BoxDecoration(
-        //             color: Colors.blueGrey,
-        //           ),
-        //           child: const Center(
-        //               child: Text(
-        //             "what should go here?",
-        //             style: TextStyle(color: Colors.white),
-        //           ))),
-        //       panel: FormWidget(),
-        //     ),
-
-
-
-        //     //THESE ARE THE INDIVIDUAL POST AND POST CREATION BUTTONS
-        //     // Column(
-        //     //   mainAxisAlignment: MainAxisAlignment.end,
-        //     //   children: [
-        //     //     FloatingActionButton(
-        //     //       onPressed: (() {
-        //     //         Navigator.push(
-        //     //             context,
-        //     //             MaterialPageRoute(
-        //     //                 builder: (context) => const PostPage()));
-        //     //       }),
-        //     //     ),
-        //     //   ],
-        //     // ),
-        //   ]));
-        // });
-          
-    }
-  );}
+                //     //THESE ARE THE INDIVIDUAL POST AND POST CREATION BUTTONS
+                //     // Column(
+                //     //   mainAxisAlignment: MainAxisAlignment.end,
+                //     //   children: [
+                //     //     FloatingActionButton(
+                //     //       onPressed: (() {
+                //     //         Navigator.push(
+                //     //             context,
+                //     //             MaterialPageRoute(
+                //     //                 builder: (context) => const PostPage()));
+                //     //       }),
+                //     //     ),
+                //     //   ],
+                //     // ),
+                //   ]));
+                // });
+              });
+        });
+  }
 }
