a03835e6662ad6ba906e1b5e1f204bf9a3939452
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2023-03-25 12:38:01 -0400

    resolved toby's changes

--- lib/listPage.dart
+++ lib/listPage.dart
@@ -1,53 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:lost_found_steelhacks/lostAndFoundObject.dart';
-<<<<<<< HEAD
 import 'package:lost_found_steelhacks/postPage.dart';
 
 class listPage extends StatefulWidget {
   final double spacing = 10;
   final double border_radius = 5;
-=======
-
-class listPage extends StatefulWidget {
-  final double spacing = 10;
->>>>>>> 00a627329f86ee83ef09313a241d5cbeea81181c
   final List<LostAndFoundObject> entries;
   const listPage({super.key, required this.entries});
 
   List<LostAndFoundObject> getEntries() {
     return entries;
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 00a627329f86ee83ef09313a241d5cbeea81181c
   @override
   State<listPage> createState() => _listPageState();
 }
 
 class _listPageState extends State<listPage> {
-<<<<<<< HEAD
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Lost and Found"),
         ),
         body: ListView(children: get_list_entries()));
-=======
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: get_list_entries()
-      )
-    );
->>>>>>> 00a627329f86ee83ef09313a241d5cbeea81181c
   }
 
   List<Widget> get_list_entries() {
     List<Widget> containers = [];
     for (LostAndFoundObject e in widget.entries) {
-<<<<<<< HEAD
       // Padding
       containers.add(Padding(padding: EdgeInsets.all(widget.spacing)));
       // Entry
@@ -74,23 +54,3 @@ class _listPageState extends State<listPage> {
     return containers;
   }
 }
-=======
-      containers.add(
-        Container(
-          height: 100,
-          color: Color.fromARGB(255, 14, 165, 192),
-          child: ListView(children: [
-            Text(e.itemName.toString()),
-            Text(e.phone.toString()),
-            Text(e.description.toString())
-          ]) 
-        )
-      );
-      containers.add(
-        Padding(padding: EdgeInsets.all(widget.spacing))
-      );
-    };
-    return containers;
-  }
-}
->>>>>>> 00a627329f86ee83ef09313a241d5cbeea81181c
